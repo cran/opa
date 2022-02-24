2dfc412ac928c58e3633a8da39ff310a17b92a96
Timothy Beechey <tim.beechey@protonmail.com>
2022-02-24 07:30:02 +0000

    version 0.1.0

---
+++ R/fitopa.R
@@ -0,0 +1,202 @@
+#' Fit an ordinal pattern analysis model
+#'
+#' \code{opa} is used to fit ordinal pattern analysis models by computing the
+#' percentage of pair orderings in each row of data which are matched by
+#' corresponding pair orderings in an hypothesis, in addition the chance of a
+#' permutation of the data producing a percentage match as great.
+#'
+#' @details
+#' Data is expected in \strong{wide} format with 1 row per individual and 1
+#' column per measurement condition. Data must contain only columns consisting
+#' of numerical values of the \emph{dependent} variable.
+#'
+#' The length of the \code{hypothesis} must be equal to the number of columns in
+#' the dependent variable data.frame \code{dat}.
+#'
+#' Any \emph{independent} variable must be passed separately as a vector with the
+#' \code{group} keyword. The grouping vector must be a \emph{factor}.
+#'
+#' \code{pairing_type} must be either "pairwise" or "adjacent". The "pairwise"
+#' option considered the relative ordering of every pair of observations in
+#' the data and every pair of elements of the hypothesis. The "adjacent" option
+#' considered the ordering of adjacent pairs only. If unspecified, the default
+#' is "pairwise".
+#'
+#' \code{diff_threshold} may be a positive integer or double. If unspecified
+#' a default zero threshold is used. The \code{diff_threshold} is never applied
+#' to the hypothesis.
+#'
+#' \code{cval_method} is either "stochastic" or "exact". The "stochastic" option
+#' generates random reorderings of each data row. The "exact" method generates
+#' every possible permutation of each data row. Care must be taken using the
+#' "exact" method since the number of permutations is the factorial of the
+#' number of columns in the data. For large numbers of data columns it is best
+#' to use the default "stochastic" method to sample orderings.
+#'
+#' \code{nreps} specifies the number of random reorderigs to generate when
+#' using the "stochastic" method for computing chance values. The default
+#' value of \code{nreps} is 1000. If the \code{cval_method = "exact"} option
+#' is specified, \code{nreps} is ignored.
+#'
+#' @references
+#' Grice, J. W., Craig, D. P. A., & Abramson, C. I. (2015). A Simple and
+#' Transparent Alternative to Repeated Measures ANOVA. SAGE Open, 5(3),
+#' 215824401560419. <https://doi.org/10.1177/2158244015604192>
+#'
+#' Thorngate, W. (1987). Ordinal Pattern Analysis: A Method for Assessing
+#' Theory-Data Fit. Advances in Psychology, 40, 345–364.
+#' <https://doi.org/10.1016/S0166-4115(08)60083-7>
+#'
+#' @param dat a data frame
+#' @param hypothesis a numeric vector
+#' @param group an optional factor vector
+#' @param pairing_type a string
+#' @param diff_threshold a positive integer or floating point number
+#' @param cval_method a string, either "exact" or "stochastic
+#' @param nreps an integer, ignored if \code{cval_method = "exact"}
+#' @return \code{opa} returns an object of class "opafit".
+#'
+#' An object of class "opafit" is a list containing the folllowing components:
+#' \describe{
+#'   \item{group_pcc}{the percentage of pairwise orderings from all pooled data
+#'   rows which were correctly classified by the hypothesis.}
+#'   \item{individual_pccs}{a vector containing the percentage of pairwise
+#'   orderings that were correctly classified by the hypothesis for each data
+#'   row.}
+#'   \item{correct_pairs}{an integer representing the number of pairwise
+#'   orderings pooled across all data rows that were correctly classified by the
+#'   hypothesis.}
+#'   \item{total_pairs}{an integer, the number of pair orderings contained in
+#'   the data.}
+#'   \item{group_cval}{the group-level chance value.}
+#'   \item{individual_cvals}{a vector containing chance values for each data
+#'   row}
+#'   \item{n_permutations}{an integer, the number of permutations of the data
+#'   used to compute chance values.}
+#'   \item{pccs_geq_observed}{an integer, the number of permutations which
+#'   generated PCC values at least as great as the PCC of the observed data.}
+#'   \item{pcc_replicates}{a matrix containing PCC values, one column per data
+#'   row, computed from all permutations used to compute chance values. }
+#'   \item{call}{the matched call}
+#'   }
+#' @examples
+#' dat <- data.frame(group = c("a", "b", "a", "b"),
+#'                   t1 = c(9, 4, 8, 10),
+#'                   t2 = c(8, 8, 12, 10),
+#'                   t3 = c(8, 5, 10, 11))
+#' dat$group <- factor(dat$group, levels = c("a", "b"))
+#' opamod <- opa(dat[,2:4], 1:3)
+#' opa(dat[,2:4], 1:3)
+#' opa(dat[,2:4], 1:3, nreps = 500)
+#' opa(dat[,2:4], 1:3, cval_method = "exact")
+#' opa(dat[,2:4], 1:3, pairing_type = "adjacent")
+#' opa(dat[,2:4], 1:3, diff_threshold = 1)
+#' opa(dat[,2:4], 1:3, group = dat$group)
+#' @export
+opa <- function(dat, hypothesis, group = NULL, pairing_type = "pairwise",
+                diff_threshold = 0, cval_method = "stochastic", nreps = 1000) {
+  # verify the arguments
+  assertthat::assert_that(assertthat::are_equal(ncol(dat), length(hypothesis)))
+  assertthat::assert_that(pairing_type %in% c("pairwise", "adjacent"))
+  assertthat::assert_that(cval_method %in% c("exact", "stochastic"))
+  assertthat::assert_that(class(diff_threshold) %in% c("integer", "numeric"))
+  assertthat::assert_that(assertthat::is.count(nreps))
+  assertthat::assert_that(assertthat::is.scalar(diff_threshold))
+  assertthat::assert_that(assertthat::is.count(nreps))
+  assertthat::assert_that(assertthat::are_equal(diff_threshold >= 0, TRUE))
+
+  if (is.null(group)) { # single groups
+    # convert the data.frame input to a matrix for speed
+    mat <- as.matrix(dat)
+
+    pccs <- pcc(mat, hypothesis, pairing_type, diff_threshold)
+    if (cval_method == "exact") {
+      cvalues <- cval_exact(pccs)
+    } else if (cval_method == "stochastic") {
+      cvalues <- cval_stochastic(pccs, nreps)
+    }
+
+    return(
+      structure(
+        list(group_pcc = pccs$group_pcc,
+             individual_pccs = pccs$individual_pccs,
+             correct_pairs = pccs$correct_pairs,
+             total_pairs = pccs$total_pairs,
+             group_cval = cvalues$group_cval,
+             individual_cvals = cvalues$individual_cvals,
+             n_permutations = cvalues$total_perms,
+             pccs_geq_observed = cvalues$perm_pccs_geq_obs_pcc,
+             pcc_replicates = cvalues$pcc_replicates,
+             call = match.call(),
+             hypothesis = hypothesis,
+             pairing_type = pairing_type,
+             diff_threshold = diff_threshold,
+             cval_method = cval_method,
+             data = dat,
+             groups = group),
+        class = "opafit"))
+
+  } else { # multiple groups
+    assertthat::assert_that(is.factor(group) == TRUE, msg = "The grouping vector must be a factor.")
+    groups <- levels(group)
+    group_pccs <- numeric(nlevels(group))
+    group_cvals <- numeric(nlevels(group))
+    individual_pccs <- numeric(0)
+    individual_cvals <- numeric(0)
+    individual_idx <- numeric(0)
+    group_labels_vec <- character(0)
+    correct_pairs <- 0
+    total_pairs <- 0
+    n_permutations <- 0
+    pccs_geq_observed <- 0
+    pcc_replicates <- vector(nlevels(group), mode="list")
+
+    for (i in 1:nlevels(group)) {
+      idx <- which(group == groups[i])
+      subgroup_dat <- dat[idx,]
+      subgroup_mat <- as.matrix(subgroup_dat)
+      subgroup_pccs <- pcc(subgroup_mat, hypothesis, pairing_type, diff_threshold)
+      cat("Fitting group", i, "of", nlevels(group), "\n")
+      if (cval_method == "exact") {
+        subgroup_cvalues <- cval_exact(subgroup_pccs)
+      } else if (cval_method == "stochastic") {
+        subgroup_cvalues <- cval_stochastic(subgroup_pccs, nreps)
+      }
+      group_pccs[i] <- subgroup_pccs$group_pcc
+      correct_pairs <- correct_pairs + subgroup_pccs$correct_pairs
+      total_pairs <- total_pairs + subgroup_pccs$total_pairs
+      group_cvals[i] <- subgroup_cvalues$group_cval
+      pcc_replicates[[i]] <- subgroup_cvalues$pcc_replicates
+      n_permutations <- n_permutations + subgroup_cvalues$total_perms
+      pccs_geq_observed <- pccs_geq_observed + subgroup_cvalues$perm_pccs_geq_obs_pcc
+      individual_idx <- append(individual_idx, idx)
+      group_labels_vec <- append(group_labels_vec, rep(groups[i], length(idx)))
+      individual_pccs <- append(individual_pccs, subgroup_pccs$individual_pccs)
+      individual_cvals <- append(individual_cvals, subgroup_cvalues$individual_cvals)
+    }
+    names(group_pccs) <- levels(group)
+    names(group_cvals) <- levels(group)
+
+    return(
+      structure(
+        list(group_pcc = group_pccs,
+             individual_pccs = individual_pccs,
+             correct_pairs = correct_pairs,
+             total_pairs = total_pairs,
+             group_cval = group_cvals,
+             individual_cvals = individual_cvals,
+             individual_idx = individual_idx,
+             group_labels = group_labels_vec,
+             n_permutations = n_permutations,
+             pccs_geq_observed = pccs_geq_observed,
+             pcc_replicates = pcc_replicates,
+             call = match.call(),
+             hypothesis = hypothesis,
+             pairing_type = pairing_type,
+             diff_threshold = diff_threshold,
+             cval_method = cval_method,
+             data = dat,
+             groups = group),
+        class = "opafit"))
+  }
+}
---
+++ R/utility.R
@@ -0,0 +1,259 @@
+# Generates pairwise ordinal relations from a vector, consisting of integers
+# from the set {1, 0, -1}. When the pairing_type = "adjacent" option is used,
+# calling ordering() on a vector of length N produces a vector of length N-1.
+# When the pairing_type = "pairwise" option is used, calling ordering() on an
+# N-length vector returns a vector of length ((N-1) * N)/2
+# param: xs a numeric vector
+# param: pairing_type a character string, either "adjacent" or "pairwise"
+# param: diff_threshold: a numeric scalar
+# return: a numeric vector
+ordering <- function(xs, pairing_type, diff_threshold) {
+  if (pairing_type == "pairwise") {
+    c_sign_with_threshold(c_all_diffs(xs), diff_threshold)
+  } else if (pairing_type == "adjacent") {
+    c_sign_with_threshold(diff(xs), diff_threshold)
+  }
+}
+
+# Removes elements of a hypothesis vector that correspond to the position of
+# NAs in a numeric vector of data (a data row).
+# param: xs a numeric vector
+# param: h a numeric vector
+# return: a numeric vector
+conform <- function(xs, h) {
+  h[-which(is.na(xs))]
+}
+
+#' Prints a summary of results from a fitted ordinal pattern analysis model.
+#' @param object an object of class "opafit".
+#' @param digits an integer used for rounding values in the output.
+#' @param ... ignored
+#' @return No return value, called for side effects.
+#' @examples
+#' dat <- data.frame(t1 = c(9, 4, 8, 10),
+#'                   t2 = c(8, 8, 12, 10),
+#'                   t3 = c(8, 5, 10, 11))
+#' opamod <- opa(dat, 1:3)
+#' summary(opamod)
+#' summary(opamod, digits = 3)
+#' @export
+summary.opafit <- function(object, ..., digits = 2L) {
+  if (is.null(object$groups)) {
+    cat("Ordinal Pattern Analysis of", ncol(object$data), "observations for",
+        nrow(object$data), "individuals in 1 group \n\n")
+  } else {
+    cat("Ordinal Pattern Analysis of", ncol(object$data), "observations for",
+        nrow(object$data), "individuals in", nlevels(object$groups), "groups \n\n")
+  }
+  cat("Group-level results:\n")
+  print(group_results(object, digits))
+  cat("\nIndividual-level results:\n")
+  print(individual_results(object, digits))
+  cat("\nPCCs were calculated for ", object$pairing_type,
+      " ordinal relationships using a difference threshold of ", object$diff_threshold,
+      ".\n", sep="")
+  cat("Chance-values were calculated using the", object$cval_method, "method.\n")
+}
+
+#' @export
+print.opafit <- function(x, ...) {
+  print(x$call)
+}
+
+#' Plots individual PCCs relative to a user-supplied PCC threshold value.
+#' @param m an object of class "opafit"
+#' @param pcc_threshold a numeric scalar
+#' @return an object of class "ggplot"
+#' @examples
+#' dat <- data.frame(t1 = c(9, 4, 8, 10),
+#'                   t2 = c(8, 8, 12, 10),
+#'                   t3 = c(8, 5, 10, 11))
+#' opamod <- opa(dat, 1:3)
+#' pcc_threshold_plot(opamod)
+#' pcc_threshold_plot(opamod, pcc_threshold = 85)
+#' @export
+pcc_threshold_plot <- function(m, pcc_threshold = 75) {
+  Individual <- group <- PCC <- NULL # bind variables to function
+  if (is.null(m$groups)) { # single group
+    df <- data.frame(Individual = 1:nrow(m$data),
+                     PCC = m$individual_pccs)
+    ggplot2::ggplot(df, ggplot2::aes(x = Individual, y = PCC)) +
+      ggplot2::scale_x_continuous(breaks = 1:length(m$individual_pccs)) +
+      ggplot2::geom_hline(yintercept = pcc_threshold, linetype = 2, colour = "red") +
+      ggplot2::geom_segment(ggplot2::aes(x=Individual, xend=Individual, y=0, yend=PCC), colour="black", size=0.3) +
+      ggplot2::geom_point(size=2, shape=21, fill="royalblue") +
+      ggplot2::guides(fill="none") +
+      ggplot2::coord_flip() +
+      ggplot2::theme(panel.grid.major.y = ggplot2::element_blank(),
+                     panel.grid.minor.y = ggplot2::element_blank())
+  } else { # multiple groups
+    df <- data.frame(Individual = 1:length(m$individual_pccs),
+                     group = factor(m$group_labels),
+                     PCC = m$individual_pccs)
+    ggplot2::ggplot(df, ggplot2::aes(x = Individual, y = PCC)) +
+      ggplot2::scale_x_continuous(breaks = 1:length(m$individual_idx), labels=m$individual_idx) +
+      ggplot2::geom_hline(yintercept = pcc_threshold, linetype = 2, colour = "red") +
+      ggplot2::geom_segment(ggplot2::aes(x=Individual, xend=Individual, y=0, yend=PCC), colour="black", size=0.3) +
+      ggplot2::geom_point(size=2, shape=21, ggplot2::aes(fill=group)) +
+      ggplot2::coord_flip() +
+      ggplot2::theme(panel.grid.major.y = ggplot2::element_blank(),
+                     panel.grid.minor.y = ggplot2::element_blank(),
+                     legend.position = "bottom")
+  }
+}
+
+#' Plots individual-level PCCs and chance-values.
+#' @param x an object of class "opafit" produced by \code{opa()}
+#' @param ... ignored
+#' @return an object of class "ggplot"
+#' @examples
+#' dat <- data.frame(t1 = c(9, 4, 8, 10),
+#'                   t2 = c(8, 8, 12, 10),
+#'                   t3 = c(8, 5, 10, 11))
+#' opamod <- opa(dat, 1:3)
+#' plot(opamod)
+#' @export
+plot.opafit <- function(x, ...) {
+  Individual <- stat <- group <- value <- NULL
+  if (is.null(x$groups)) { # single group
+    df <- data.frame(Individual = rep(1:nrow(x$data), 2),
+                     stat = c(rep("PCCs", nrow(x$data)), rep("c-values", nrow(x$data))),
+                     value = c(x$individual_pccs, x$individual_cvals))
+    df$stat <- factor(df$stat, levels = c("PCCs", "c-values"))
+    ggplot2::ggplot(df, ggplot2::aes(x = Individual, y = value)) +
+      ggplot2::scale_x_continuous(breaks = 1:length(x$individual_pccs)) +
+      ggplot2::geom_segment(ggplot2::aes(x=Individual, xend=Individual, y=0, yend=value), colour="black", size=0.3) +
+      ggplot2::geom_point(size=2, shape=21, ggplot2::aes(fill=stat)) +
+      ggplot2::facet_wrap(~ stat, nrow=1, scale="free") +
+      ggplot2::ylab(NULL) +
+      ggplot2::guides(fill="none") +
+      ggplot2::coord_flip() +
+      ggplot2::theme(panel.grid.major.y = ggplot2::element_blank(),
+                     panel.grid.minor.y = ggplot2::element_blank())
+  } else { # multiple groups
+    df <- data.frame(Individual = rep(1:length(x$individual_pccs), 2),
+                     group = rep(factor(x$group_labels), 2),
+                     stat = c(rep("PCCs", nrow(x$data)), rep("c-values", nrow(x$data))),
+                     value = c(x$individual_pccs, x$individual_cvals))
+    df$stat <- factor(df$stat, levels = c("PCCs", "c-values"))
+    ggplot2::ggplot(df, ggplot2::aes(x = Individual, y = value)) +
+      ggplot2::scale_x_continuous(breaks = 1:length(x$individual_idx), labels=x$individual_idx) +
+      ggplot2::geom_segment(ggplot2::aes(x=Individual, xend=Individual, y=0, yend=value), colour="black", size=0.3) +
+      ggplot2::geom_point(size=2, shape=21, ggplot2::aes(fill=group)) +
+      ggplot2::facet_wrap(~ stat, nrow=1, scale="free") +
+      ggplot2::ylab(NULL) +
+      ggplot2::coord_flip() +
+      ggplot2::theme(panel.grid.major.y = ggplot2::element_blank(),
+                     panel.grid.minor.y = ggplot2::element_blank(),
+                     legend.position = "bottom")
+  }
+}
+
+#' Returns group-level PCC and chance values.
+#'
+#' @details
+#' If the model was fitted with no grouping variable, a single PCC and c-value
+#' are returned. If a grouping variable was specified in the call to \code{opa}
+#' then PCCs and c-values are returned for each factor level of the grouping
+#' variable.
+#' @param m an object of class "opafit" produced by \code{opa()}.
+#' @param digits a positive integer.
+#' @return a matrix with 1 row per group.
+#' @examples
+#' dat <- data.frame(t1 = c(9, 4, 8, 10),
+#'                   t2 = c(8, 8, 12, 10),
+#'                   t3 = c(8, 5, 10, 11))
+#' opamod <- opa(dat, 1:3)
+#' group_results(opamod)
+#' @export
+group_results <- function(m, digits) {
+  UseMethod("group_results")
+}
+
+#' @rdname group_results
+#' @export
+group_results.default <- function(m, digits) .NotYetImplemented()
+
+#' @rdname group_results
+#' @export
+group_results.opafit <- function(m, digits = 2) {
+  if (is.null(m$groups)) {
+    out <- matrix(c(round(m$group_pcc, digits), round(m$group_cval, digits)),
+                  nrow = 1)
+    colnames(out) <- c("PCC", "cval")
+    rownames(out) <- "pooled"
+    return(out)
+  }
+  else {
+    out <- cbind(round(m$group_pcc, digits), round(m$group_cval, digits))
+    colnames(out) <- c("PCC", "cval")
+    rownames(out) <- levels(m$groups)
+    return(out)
+  }
+}
+
+#' Returns individual-level PCC and chance values.
+#'
+#' @details
+#' If the model was fitted with no grouping variable, a matrix of PCCs and
+#' c-values are returned corresponding to the order of rows in the data. If the
+#' \code{opa} model was fitted with a grouping variable specified, a table of
+#' PCCs and c-values is returned ordered by factor level of the grouping
+#' variable.
+#' @param m an object of class "opafit" produced by \code{opa()}
+#' @param digits an integer
+#' @return a matrix containing a column of PCC values and a column of c-values
+#' with 1 row per row of data.
+#' @examples
+#' dat <- data.frame(t1 = c(9, 4, 8, 10),
+#'                   t2 = c(8, 8, 12, 10),
+#'                   t3 = c(8, 5, 10, 11))
+#' opamod <- opa(dat, 1:3)
+#' individual_results(opamod)
+#' @export
+individual_results <- function(m, digits) {
+  UseMethod("individual_results")
+}
+
+#' @rdname individual_results
+#' @export
+individual_results.default <- function(m, digits) .NotYetImplemented()
+
+#' @rdname individual_results
+#' @export
+individual_results.opafit <- function(m, digits = 2) {
+  if (is.null(m$groups)) {
+    out <- round(cbind(m$individual_pccs, m$individual_cvals), digits)
+    colnames(out) <- c("PCC", "cval")
+    rownames(out) <- seq(length(m$individual_pccs))
+    return(out)
+  } else {
+    out <- round(cbind(m$individual_idx, m$individual_pccs, m$individual_cvals), digits)
+    colnames(out) <- c("Individual", "PCC", "cval")
+    rownames(out) <- m$group_labels
+    return(out)
+  }
+}
+
+#' Convenience function for plotting a hypothesis.
+#' @param h a numeric vector
+#' @return an object of class "ggplot"
+#' @examples
+#' my_hypothesis <- c(1,2,3,3,3)
+#' plot_hypothesis(my_hypothesis)
+#' @export
+plot_hypothesis <- function(h) {
+  condition <- hypothesis <- NULL
+  df <- data.frame(condition = 1:length(h), hypothesis = h)
+  ggplot2::ggplot(df, ggplot2::aes(x = condition, y = hypothesis)) +
+    ggplot2::geom_point(size = 4, shape = 21, fill = "royalblue") +
+    ggplot2::scale_x_continuous(labels = as.character(df$condition), breaks = df$condition) +
+    ggplot2::scale_y_continuous(labels = as.character(df$hypothesis), breaks = df$hypothesis) +
+    ggplot2::labs(x = "Condition", y = "Relative Value") +
+    ggplot2::theme(panel.grid.minor = ggplot2::element_blank())
+}
+
+# Clean up C++ when package is unloaded.
+.onUnload <- function(libpath) {
+  library.dynam.unload("opa", libpath)
+}
